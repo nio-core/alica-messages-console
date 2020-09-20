ccb9331d479212b226495db20b792efdaab3a360
Sven Starcke <sst97@gmx.net>
2020-09-20 21:45:42 +0200

    Introduces subcommand for creation of new transaction to cli

--- src/lib.rs
+++ src/lib.rs
@@ -47,35 +47,42 @@ pub fn get_commandline_arguments<'a>() -> clap::ArgMatches<'a> {
                 .short("C")
                 .long("connect")
                 .takes_value(true)
+                .required(true)
                 .help("ZeroMQ address of a validator"),
         )
-        .arg(
-            clap::Arg::with_name("agent id")
-                .short("i")
-                .long("id")
-                .takes_value(true)
-                .help("The unique identifier of the sending agent"),
-        )
-        .arg(
-            clap::Arg::with_name("message type")
-                .short("t")
-                .long("type")
-                .takes_value(true)
-                .help("The type of the message to log"),
-        )
-        .arg(
-            clap::Arg::with_name("message")
-                .short("m")
-                .long("message")
-                .takes_value(true)
-                .help("The message to log"),
-        )
-        .arg(
-            clap::Arg::with_name("timestamp")
-                .short("z")
-                .long("timestamp")
-                .takes_value(true)
-                .help("The timestamp of the moment the message was recorded"),
-        )
+        .subcommand(clap::SubCommand::with_name("new")
+            .help("Adds new transaction to the chain")
+            .arg(
+                clap::Arg::with_name("agent id")
+                    .short("i")
+                    .long("id")
+                    .takes_value(true)
+                    .required(true)
+                    .help("The unique identifier of the sending agent"),
+            )
+            .arg(
+                clap::Arg::with_name("message type")
+                    .short("t")
+                    .long("type")
+                    .takes_value(true)
+                    .required(true)
+                    .help("The type of the message to log"),
+            )
+            .arg(
+                clap::Arg::with_name("message")
+                    .short("m")
+                    .long("message")
+                    .takes_value(true)
+                    .required(true)
+                    .help("The message to log"),
+            )
+            .arg(
+                clap::Arg::with_name("timestamp")
+                    .short("z")
+                    .long("timestamp")
+                    .required(true)
+                    .takes_value(true)
+                    .help("The timestamp of the moment the message was recorded"),
+            ))
         .get_matches()
 }
