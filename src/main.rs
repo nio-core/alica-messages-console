0c771adee1692e04ec784e0f370718be7c810c9f
Sven Starcke <sst97@gmx.net>
2020-11-15 01:02:37 +0100

    Cleans main method

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,34 @@ use alica_messages_client::get_commandline_arguments;
 use alica_messages_client::command::SawtoothCommand;
 use alica_messages_client::command::transaction;
 use alica_messages_client::command::state;
-use alica_messages_client::sawtooth::{AlicaMessage, Client, TransactionFamily};
+use alica_messages_client::sawtooth::{self, AlicaMessage, TransactionFamily};
 use alica_messages_client::sawtooth::factory::GeneralPurposeComponentFactory;
-
-fn alica_message_from(args: &clap::ArgMatches) -> AlicaMessage {
-    AlicaMessage::new(
-        args.value_of("agent id").unwrap().to_string(),
-        args.value_of("message type").unwrap().to_string(),
-        args.value_of("message").unwrap().to_string(),
-        args.value_of("timestamp").unwrap().to_string()
-    )
-}
 
 fn main() {
     let args = get_commandline_arguments();
-    let validator_url = args.value_of("connect").unwrap();
-
-    let transaction_family = TransactionFamily::new("alica_messages", "0.1.0");
-    let component_factory = GeneralPurposeComponentFactory::new(transaction_family);
-    let client = Client::new(validator_url, Box::from(component_factory));
-
-    let (subcommand, subcommand_args) = args.subcommand();
-    let command: Box<dyn SawtoothCommand> = match subcommand {
-        "new" => {
-            let args = match subcommand_args {
-                Some(args) => args,
-                None => {
-                    println!("No parameters supplied for transaction addition");
-                    return;
-                }
-            };
-
-            Box::new(transaction::SubmissionCommand::new(&client, alica_message_from(args)))
-        },
-        "list" => {
-            Box::new(transaction::ListCommand::new(&client))
-        },
-        "state" => {
-            Box::new(state::ListCommand::new(&client, ""))
-        },
-        _ => {
-            println!("No subcommand supplied");
-            return;
-        }
+    let client = create_client_from(&args);
+    let command: Box<dyn SawtoothCommand> = match args.subcommand() {
+        ("new", Some(args)) => Box::new(transaction::SubmissionCommand::new(&client, alica_message_from(&args))),
+        ("list", None) => Box::new(transaction::ListCommand::new(&client)),
+        ("state", None) => Box::new(state::ListCommand::new(&client, "")),
+        _ => panic!("No subcommand supplied")
     };
 
     command.execute().expect("Command execution failed");
+}
+
+fn create_client_from<'a>(args: &clap::ArgMatches) -> sawtooth::Client<'a> {
+    let validator_url = args.value_of("connect").expect("Validator address missing");
+    let transaction_family = TransactionFamily::new("alica_messages", "0.1.0");
+    let component_factory = GeneralPurposeComponentFactory::new(transaction_family);
+    sawtooth::Client::new(validator_url, Box::from(component_factory))
+}
+
+fn alica_message_from(args: &clap::ArgMatches) -> AlicaMessage {
+    AlicaMessage::new(
+        args.value_of("agent id").expect("agent id missing").to_string(),
+        args.value_of("message type").expect("message type missing").to_string(),
+        args.value_of("message").expect("message missing").to_string(),
+        args.value_of("timestamp").expect("timestamp missing").to_string()
+    )
 }
