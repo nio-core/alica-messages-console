5e99e68c474e62ed7a3070e89ec8aaba106e2624
Sven Starcke <sst97@gmx.net>
2020-11-16 21:53:52 +0100

    Moves batch creation command to own module

--- src/command/transaction.rs
+++ src/command/transaction.rs
@@ -1,27 +1,6 @@
-use crate::sawtooth::{AlicaMessagePayload, Client};
+use crate::sawtooth::Client;
 use crate::command::{SawtoothCommand, ExecutionResult};
 use crate::command::Error::{ClientError};
-
-pub struct BatchCreationCommand {
-    client: Client,
-    message: AlicaMessagePayload
-}
-
-impl BatchCreationCommand {
-    pub fn new(client: Client, message: AlicaMessagePayload) -> Self {
-        BatchCreationCommand {
-            client,
-            message
-        }
-    }
-}
-
-impl SawtoothCommand for BatchCreationCommand {
-    fn execute(&self) -> ExecutionResult {
-        let messages = vec![&self.message];
-        self.client.create_batch(&messages).map_err(|_| ClientError)
-    }
-}
 
 pub struct ListCommand {
     client: Client
